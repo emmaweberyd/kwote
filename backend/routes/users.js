6e68c5959e4d60ae5728f583b25ef29828bebd42
emmaweberyd <emmwe942@student.liu.se>
2019-10-04 14:52:19 +0200

    feat: add validation to be handled before add-user request

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -8,20 +8,34 @@ router.route('/').get((req,res) => {
 });
 
 router.route('/add').post((req,res) => {
-    const firstname = req.body.firstname;
-    const lastname = req.body.lastname;
-    const email = req.body.email;
-    const password = req.body.password;
-    const newUser = new User({
-        firstname, 
-        lastname, 
-        email,
-        password
-    });
+    const {firstname, lastname, email, password} = req.body;
 
-    newUser.save()
-        .then(() => res.json('User added!'))
-        .catch(err => res.status(400).json('Error: ' + err));
+    // make sure all fields are non-empty
+    if(!firstname || !lastname || !email || !password){
+        return res.status(400).json({msg: 'Enter all fields'});
+    }
+
+    // make sure password is long enough
+    if(password.length < 8){
+        return res.status(400).json({msg: 'Password needs at least 8 characters'});
+    }
+
+    // create user if doesn't already exist
+    User.findOne({email})
+        .then(user =>{
+            if(user) return res.status(400).json({msg: 'User already exists'});
+
+            const newUser = new User({
+                firstname, 
+                lastname, 
+                email,
+                password
+            });
+
+            newUser.save()
+            .then(() => res.json({msg: 'User added!'}))
+            .catch(err => res.status(400).json('Error: ' + err));
+        });
 });
 
 module.exports = router;
