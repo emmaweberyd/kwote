afd45b1cab8826b906a01beb11c8e61af6265c8c
emmaweberyd <emmwe942@student.liu.se>
2019-10-07 20:48:54 +0200

    feat: add frontend paths to login and register pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import RegisterUser from './components/register-user.component';
+import LoginUser from './components/login-user.component';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="container">
+        <Route path="/register" component={RegisterUser}/>
+        <Route path="/login" component={LoginUser}/>
+      </div>
+    </Router>
   );
 }
 
