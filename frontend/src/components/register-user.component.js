fe3d958bb7b773cec20a3e85db9c6c79d5f20886
emmaweberyd <emmwe942@student.liu.se>
2019-10-07 15:31:03 +0200

    feat: create basic form for registering users

---
+++ frontend/src/components/register-user.component.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+
+export default class RegisterUser extends Component {
+    constructor(props){
+        super(props);
+
+        this.onChangeFirstname = this.onChangeFirstname.bind(this);
+        this.onChangeLastname = this.onChangeLastname.bind(this);
+        this.onChangeEmail = this.onChangeEmail.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            firstname: '',
+            lastname: '',
+            email: '',
+            password: ''
+        }
+    }
+
+    onChangeFirstname(e) {
+        this.setState({
+            firstname: e.target.value
+        })
+    }
+
+    onChangeLastname(e) {
+        this.setState({
+            lastname: e.target.value
+        })
+    }
+
+    onChangeEmail(e) {
+        this.setState({
+            email: e.target.value
+        })
+    }
+
+    onChangePassword(e) {
+        this.setState({
+            password: e.target.value
+        })
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const user = {
+            firstname: this.state.firstname,
+            lastname: this.state.lastname,
+            email: this.state.email,
+            password: this.state.password
+        }
+
+        console.log(user)
+    }
+
+
+    render(){
+        return (
+            <div>
+                <h3>Register User!</h3>
+                <form onSubmit={this.onSubmit}>
+                    <label>Firstname: </label>
+                    <input 
+                        required 
+                        value={this.state.firstname}
+                        onChange={this.onChangeFirstname}
+                        />
+                    <label>Lastname: </label>
+                    <input 
+                        required 
+                        value={this.state.lastname}
+                        onChange={this.onChangeLastname}
+                        />
+                    <label>Email: </label>
+                    <input 
+                        required 
+                        value={this.state.email}
+                        onChange={this.onChangeEmail}
+                        />
+                    <label>Password: </label>
+                    <input 
+                        required 
+                        value={this.state.password}
+                        onChange={this.onChangePassword}
+                        />
+                    <button
+                        onClick={this.onSubmit}
+                        >
+                        Submit
+                    </button>
+                </form>
+            </div>
+        )
+    }
+}
+
