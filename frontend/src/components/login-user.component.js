89e5f5620d15c538b387355f15df9397e412d7f9
emmaweberyd <emmwe942@student.liu.se>
2019-11-01 19:22:03 +0100

    refactor: use validation frameworks for form validation

--- frontend/src/components/login-user.component.js
+++ frontend/src/components/login-user.component.js
@@ -1,78 +1,80 @@
 import React, { Component } from 'react';
 import { Button, Form, FormGroup, FormControl, FormLabel } from "react-bootstrap";
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 import axios from 'axios';
 
-
 export default class LoginUser extends Component {
-    constructor(props){
-        super(props);
-
-        this.onChangeEmail = this.onChangeEmail.bind(this);
-        this.onChangePassword = this.onChangePassword.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-
-        this.state = {
-            email: '',
-            password: ''
-        }
-    }
-
-    onChangeEmail(e) {
-        this.setState({
-            email: e.target.value
-        })
-    }
-
-    onChangePassword(e) {
-        this.setState({
-            password: e.target.value
-        })
-    }
-
-    onSubmit(e) {
-        e.preventDefault();
-
-        const user = {
-            email: this.state.email,
-            password: this.state.password
-        }
-
-        axios.post('http://localhost:5000/users/login', user)
-            .then(res => console.log(res.data))
-            .catch(error => console.log(error));
-
-    }
-
-    render(){
+    render () {
         return (
             <div>
-                <h3 style={{paddingTop: '100px', paddingBottom: '40px'}}>Login User!</h3>
-                <Form onSubmit={this.onSubmit}>
-                    <FormGroup size="large">
-                        <FormLabel>Email</FormLabel>
-                        <FormControl
-                            autoFocus
-                            required 
-                            type="email"
-                            value={this.state.email}
-                            onChange={this.onChangeEmail}
-                    />
-                    </FormGroup>
-                    <FormGroup size="large">
-                        <FormLabel>Password</FormLabel>
-                        <FormControl
-                            required 
-                            value={this.state.password}
-                            onChange={this.onChangePassword}
-                            type="password"
-                        />
-                    </FormGroup>
-                    <Button block size="large" type="submit">
-                    Login
-                    </Button>
-                </Form>
+                <h3 style={{paddingTop: '100px', paddingBottom: '40px'}}>Log in!</h3>
+                <LoginForm/>
             </div>
         )
     }
-
 }
+
+const LoginForm = () => {
+    const formik = useFormik({
+        initialValues: {
+          email: '',
+          password: '',
+        },
+        validationSchema: yup.object({
+            email: yup.string()
+                .required('Email is required'),
+            password: yup.string()
+                .required('No password provided.')
+        }),
+        onSubmit: values => {
+            const user = {
+                email: values.email,
+                password: values.password
+            }
+            axios.post('http://localhost:5000/users/login', user)
+                .then(res => console.log(res.data))
+                .catch(error => console.log(error));
+        },
+    });
+    return (
+        <Form 
+            noValidate 
+            onSubmit={formik.handleSubmit}
+        >
+            <FormGroup size="large">
+                <FormLabel>Email</FormLabel>
+                <FormControl
+                    autoFocus
+                    required 
+                    type="email"
+                    name="email"
+                    value={formik.values.email || ''}
+                    onChange={formik.handleChange}
+                    isInvalid={formik.touched.email && formik.errors.email}
+                />
+                <Form.Control.Feedback type="invalid">
+                    {formik.errors.email}
+                </Form.Control.Feedback>
+            </FormGroup>
+            <FormGroup size="large">
+                <FormLabel>Password</FormLabel>
+                <FormControl
+                    autoFocus
+                    required 
+                    type="password"
+                    name="password"
+                    value={formik.values.password || ''}
+                    onChange={formik.handleChange}
+                    isInvalid={formik.touched.password && formik.errors.password}
+                />
+                <Form.Control.Feedback type="invalid">
+                    {formik.errors.password}
+                </Form.Control.Feedback>
+            </FormGroup>
+            <Button block size="large" type="submit">
+                Sign up
+            </Button>
+        </Form>
+    );
+};
