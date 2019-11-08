8e5d5f077ded34a403706809ece0491e73c524ae
emmaweberyd <emmwe942@student.liu.se>
2019-11-08 13:37:40 +0100

    feat: make user load when application mounts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,19 +5,28 @@ import LoginUser from './components/login-user.component';
 import { Provider } from 'react-redux';
 import store from './store';
 import Posts from './components/posts.component';
+import { loadUser } from '../src/actions/authActions';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <Provider store={store}>
-      <Router>
-        <div className="container">
-          <Route path="/main" component={Posts}/>
-          <Route path="/register" component={RegisterUser}/>
-          <Route path="/login" component={LoginUser}/>
-        </div>
-      </Router>
-    </Provider>
-  );
+
+class App extends Component {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="container">
+            <Route path="/main" component={Posts}/>
+            <Route path="/register" component={RegisterUser}/>
+            <Route path="/login" component={LoginUser}/>
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
