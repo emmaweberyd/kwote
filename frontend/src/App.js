2503752620dfec0b26f9d8dbc5bbedc729997c06
emmaweberyd <emmwe942@student.liu.se>
2019-11-09 15:33:58 +0100

    refactor: delete navbar for now

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,6 @@ import store from './store';
 import { loadUser } from '../src/actions/authActions';
 import { Component } from 'react';
 
-import Navbar from './components/navbar.component';
 import Landing from './components/landing.component';
 import RegisterUser from './components/register-user.component';
 import LoginUser from './components/login-user.component';
@@ -20,7 +19,6 @@ class App extends Component {
       <Provider store={store}>
         <Router>
           <div className="container">
-            <Navbar/>
             <Route exact path="/" component={Landing}/>
             <Route exact path="/register" component={RegisterUser}/>
             <Route exact path="/login" component={LoginUser}/>
